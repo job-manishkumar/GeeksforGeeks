44f71027573925ce5643ed574e1a63b0084b10e9
manish <118974796+job-manishkumar@users.noreply.github.com>
2025-05-31 15:25:44 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Three Great Candidates/three-great-candidates.java
@@ -0,0 +1,37 @@
+// User function Template for Java
+
+class Solution {
+    public int maxProduct(int[] arr) {
+        int min = Integer.MAX_VALUE;
+        int secondMin = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        int secondMax = Integer.MIN_VALUE;
+        int thirdMax = Integer.MIN_VALUE;
+
+        for (int i : arr) {
+            if (max < i) {
+                thirdMax = secondMax;
+                secondMax = max;
+                max = i;
+            }
+            else if (secondMax < i) {
+                thirdMax = secondMax;
+                secondMax = i;
+            }
+            else if(thirdMax< i){
+                thirdMax = i;
+            }
+            if (min > i) {
+                secondMin = min;
+                min = i;
+            } 
+            else if(secondMin > i){
+                    secondMin = i;
+            }
+        }
+
+        long getProdcutWithMin = min*secondMin*max;
+        long getProdcutWithMax = max*secondMax*thirdMax;
+        return (int)Math.max(getProdcutWithMin,getProdcutWithMax);
+    }
+}
